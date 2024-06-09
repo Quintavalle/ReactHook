8151a571843b617f65a3e521e13f4a0565835e13
Giorgio <147600523+Quintavalle@users.noreply.github.com>
2024-06-09 05:22:35 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LocationComponent from './components/LocationComponent';
+
+const App = () => (
+  <div>
+    <h1>Current Location</h1>
+    <LocationComponent />
+  </div>
+);
+
+export default App;
+
+
